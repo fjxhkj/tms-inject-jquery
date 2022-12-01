892c22613cdc6f76bc572500c17feda9e62e33db
fonny_home <fjxhkj@gmail.com>
2022-12-01 23:42:03 +0800

    更新

--- tms-inject-jquery.user.js
+++ tms-inject-jquery.user.js
@@ -1,36 +1,45 @@
 // ==UserScript==
-// @name        inject jquery
-// @version     1.0.6
+// @name        tms-inject-jquery
+// @version     1.0.7
 // @namespace   https://github.com/fjxhkj/tms-inject-jquery
 // @description 为页面注入jQuery引用节点,便于使用控制台调试jQuery选择器等.
-// @license     Apache License 2.0
-// @supportURL  https://greasyfork.org/zh-CN/scripts/395669-inject-jquery
+// @license     MIT
+// @supportURL  https://github.com/fjxhkj/tms-inject-jquery
 // @match       *
 // @include     *
 // @grant       unsafeWindow
 // @run-at      document-end
 // ==/UserScript==
 
-(function () {
-  "use strict";
+(function() {
+  'use strict';
 
-  injectJqueryNode();
-  var myReloader = setInterval(function () {
-    testJq();
-  }, 100);
-
-  function injectJqueryNode() {
-    if (typeof $ === "undefined") {
-      var node = document.createElement("script");
-      node.setAttribute("src", "https://code.jquery.com/jquery-1.12.4.js");
+  waitForEl('body', function() {
+    if ((typeof $) === 'undefined') {
+      var node = document.createElement('script');
+      node.setAttribute('src', 'http://cdn.staticfile.org/jquery/1.12.4/jquery.min.js');
       document.body.appendChild(node);
     }
+  }, 3000);
+
+  /**
+   * 等待指定CSS选择器的DOM元素出现
+   *
+   * @param selector [CSS选择器语法参考](https://www.runoob.com/cssref/css-selectors.html)
+   * @param callback 回调函数,如果超时则回调函数参数为null,否则为目标DOM元素
+   * @param maxtries 尝试次数,设为0时至少会尝试一次,设为 false 则一直尝试,设为大于0的整数则尝试该次数
+   * @param interval 尝试间隔,单位毫秒
+   */
+  function waitForEl(selector, callback, maxtries = false, interval = 100) {
+    var poller = setInterval(function() {
+      var el = document.querySelector(selector);
+      var retry = maxtries === false || maxtries-- > 0;
+      if (el === null && retry) {
+        return;
+      }
+      clearInterval(poller);
+      callback(el || null);
+    }, interval);
   }
 
-  function testJq() {
-    if (typeof $ === "function") {
-      clearInterval(myReloader);
-      console.log("tms-inject-jquery: jQuery", $.fn.jquery, "arrived!");
-    }
-  }
 })();
